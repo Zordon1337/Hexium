2cde07b0614c0018698d836f258a02eae4ae2a23
Zordon1337 <65111609+Zordon1337@users.noreply.github.com.>
2025-07-06 23:17:47 +0200

    fix forcing cursor with menu closed

--- Hexium/Hooks/WndProc.cpp
+++ Hexium/Hooks/WndProc.cpp
@@ -6,13 +6,10 @@ LRESULT WINAPI H::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     if (ImGui::GetCurrentContext()) {
         ImGuiIO& io = ImGui::GetIO();
 
-        if (ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam)) {
+        if (G::menu->IsOpen && ImGui_ImplWin32_WndProcHandler(hwnd, msg, wParam, lParam)) {
             return true; 
         }
 
-        if (io.WantCaptureKeyboard && (msg == WM_CHAR || msg == WM_KEYDOWN || msg == WM_KEYUP)) {
-            return true;
-        }
     }
 
     return CallWindowProc(H::pWndProcOG, hwnd, msg, wParam, lParam);
